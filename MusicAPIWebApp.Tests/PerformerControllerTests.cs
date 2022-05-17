23e40f885ac9066e35a0441fe50863ea217869a2
Diana Dulko <diana_dulko@knu.ua>
2022-05-17 09:38:43 +0300

    unit tests

--- MusicAPIWebApp.Tests/PerformerControllerTests.cs
+++ MusicAPIWebApp.Tests/PerformerControllerTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MusicAPIWebApp.Models;
+using MusicAPIWEbApp.Controllers;
 using Xunit;
 
 namespace MusicAPIWebApp.Tests
@@ -25,7 +26,23 @@ namespace MusicAPIWebApp.Tests
             var result = await controller.GetPerformers();
 
             // Assert
-            Assert.Contains(result.Value, g => g.Name.Equals("Антоніо Вівальді"));
+            Assert.Contains(result.Value, g => g.Name.Equals("Володимир Добринчук"));
+        }
+
+        [Fact]
+        public async void Test2()
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<MusicAPIContext>();
+            optionsBuilder.UseSqlServer("Server=DESKTOP-J8EAKAU\\SQLEXPRESS; Database=MusicAPI; Trusted_Connection=True; MultipleActiveResultSets=true");
+
+            var controller = new PerformersController(new MusicAPIContext(optionsBuilder.Options));
+
+            // Act
+            var result = await controller.GetPerformers();
+
+            // Assert
+            Assert.NotNull(result);
         }
     }
 }
